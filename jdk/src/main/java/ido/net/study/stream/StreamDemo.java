64115e61926ca05c1882334bf2d61400af739450
18065859063 <1552940727@qq.com>
2016-05-17 18:52:05 +0800

    项目初始化

---
+++ jdk/src/main/java/ido/net/study/stream/StreamDemo.java
@@ -0,0 +1,18 @@
+package ido.net.study.stream;
+
+/**
+ * Created by ido on 2016/5/17.
+ */
+
+/**
+ *  文件操作：指针操作
+ *
+ *
+ *
+ */
+public class StreamDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
